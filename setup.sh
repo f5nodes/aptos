3a05559c89f7a824dfd3c75e6776358012b75022
vasylcode <vasylcode@gmail.com>
2022-07-01 10:54:26 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+apt install unzip -y
+wget -qO aptos-cli.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-0.2.0/aptos-cli-0.2.0-Ubuntu-x86_64.zip
+unzip -o aptos-cli.zip
+chmod +x aptos
+mv aptos /usr/local/bin 
+aptos -V
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+apt-get install docker-compose
+docker -v
+docker-compose --version
+
+export WORKSPACE=testnet
+mkdir ~/$WORKSPACE
+cd ~/$WORKSPACE
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/docker-compose.yaml
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/validator.yaml
+aptos genesis generate-keys --output-dir ~/$WORKSPACE
+
+read -p "Your username: " $read_name
+read -p "Your IP: " $read_ip
+
+cd ~/$WORKSPACE
+aptos genesis set-validator-configuration \
+    --keys-dir ~/$WORKSPACE --local-repository-dir ~/$WORKSPACE \
+    --username $read_name \
+    --validator-host $read_ip:6180 \
+    --full-node-host $read_ip:6182
+
+echo "---
+root_key: "F22409A93D1CD12D2FC92B5F8EB84CDCD24C348E32B3E7A720F3D2E288E63394"
+users:
+  - "$read_name"
+chain_id: 40
+min_stake: 0
+max_stake: 100000
+min_lockup_duration_secs: 0
+max_lockup_duration_secs: 2592000
+epoch_duration_secs: 86400
+initial_lockup_timestamp: 1656615600
+min_price_per_gas_unit: 1
+allow_new_validators: true" > ~/$WORKSPACE/layout.yaml
+
+wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.2.0/framework.zip
+unzip framework.zip
+
+aptos genesis generate-genesis --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
+
+docker compose up
